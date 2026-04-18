778da2b68a60af413c8f0e208223e9cfb09046a7
Pooja-0311 <poojaprajapati123333@gmail.com>
2026-04-18 11:53:43 +0530

    add

--- Conditional/Q-10.js
+++ Conditional/Q-10.js
@@ -1,1 +1,15 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
 
+if (num1 >= num2 && num1 >= num3){
+    console.log("Largest number is :" + num1);
+}
+else if (num2 >= num1 && num2 >= num3)
+{
+    console.log("Largest number is:" + num2);
+}
+else{
+    console.log("Largest number is:" + num3);
+    
+}
